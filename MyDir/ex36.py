83d4504411b39b9faf3642884dd417e8e1e54abf
ryuneko932000 <kokeisnocturno2000@gmail.com>
2022-04-06 00:44:24 +0200

    Added ex36.py information about loops and if statements

---
+++ MyDir/ex36.py
@@ -0,0 +1,26 @@
+# Designing and debugging and small game project. 
+
+# Some quick rules regarding for and while loops:
+
+
+### IF STATEMENTS ###
+    # every 'if' statement needs a 'else' statement, if the else is just meant to not 
+    # do anything, call a function that will end the program such as dead() in ex35.py
+
+
+    # Try not to put an if statement in another if statement, if that happens use a function
+    # instead.
+ 
+    # Treat if statements like paragraphs formatting wise. That includes elif, and else
+
+    # Your boolean test should be simple.
+
+
+### LOOPS ###
+    # Use while loops only if you want something to run forever.
+    # For loops should be used for everything else, especially if there is 
+    # a fixed or limited number of things to loop over.
+
+
+
+
