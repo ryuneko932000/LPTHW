77c67165f4ad493c38e27037e906ac1f7ce876be
ryuneko932000 <kokeisnocturno2000@gmail.com>
2022-04-05 23:45:30 +0200

    Accessing elements from a list, cardinal and ordinal numbers.

--- MyDir/ex34.py
+++ MyDir/ex34.py
@@ -2,3 +2,29 @@
 # Remember that python doesn't use ordinal numbers such as: 1,2,3
 # Rather is uses cardinal ones: 0,1,2,3
 # Making it so 0 means the first element of a list.
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# the animal at 1
+print(animals[1])
+
+# the third animal 
+print(animals[2])
+
+# the first animal 
+print(animals[0])
+
+# the animal at 3
+print(animals[3])
+
+# the fifth animal 
+print(animals[4])
+
+# the animal at 2
+print(animals[2])
+
+# the sixth animal 
+print(animals[5])
+
+# the animal at 4
+print(animals[4])
